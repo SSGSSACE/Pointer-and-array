7d6aa7f834400e49d26c5e141cdaba67b5aa615d
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 22:27:40 +0700

    June 20, 2021 10:27 PM

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main()
     std::cout << "Dia chi mang array la: " << &arr << std::endl;
     std::cout << "Dia chi phan tu array 0 la: " << &arr[0] << std::endl;
     std::cout << "-------------Then---------" << std::endl;
+    std::cout << "/* message */" << std::endl;
     return 0;
 }
