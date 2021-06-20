b293049e3281d91f1eca5686ad607afde4502e97
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 22:26:32 +0700

    June 20, 2021 10:26 PM

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int arr[4] = {2, 3, 4, 5};
     int i;
+    int *p=arr;
     std::cout << "Mang array la: " << arr << std::endl;
     std::cout << "Dia chi mang array la: " << &arr << std::endl;
     std::cout << "Dia chi phan tu array 0 la: " << &arr[0] << std::endl;
