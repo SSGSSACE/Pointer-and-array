2c2bcdbebbefdc33905b2cd9624f93f3229fbce8
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 22:25:45 +0700

    June 20, 2021 10:25 PM

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int arr[4] = {2, 3, 4, 5};
+    int i;
     std::cout << "Mang array la: " << arr << std::endl;
     std::cout << "Dia chi mang array la: " << &arr << std::endl;
     std::cout << "Dia chi phan tu array 0 la: " << &arr[0] << std::endl;
